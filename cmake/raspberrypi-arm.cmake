8016a939983168c60e5979c24e5b44445fe19de6
Yong Jian Ming <jianming1993@gmail.com>
2022-01-06 22:51:22 +0800

    Fix

--- cmake/raspberrypi-arm.cmake
+++ cmake/raspberrypi-arm.cmake
@@ -1,5 +1,5 @@
 SET(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR armhf)
 
 SET(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc-10)
 SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++-10)
